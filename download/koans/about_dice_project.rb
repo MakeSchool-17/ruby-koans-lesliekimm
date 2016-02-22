7454d37d1ead4eaf6ccf8fc64f33c9cb9e8cdb5b
leslie kimm <leslie.kimm@gmail.com>
2016-02-21 20:53:22 -0800

    finish dice project

--- download/koans/about_dice_project.rb
+++ download/koans/about_dice_project.rb
@@ -3,7 +3,13 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # Implement a DiceSet Class here:
 #
 class DiceSet
+  attr_accessor :values
   def initialize
+    @values = []
+  end
+
+  def roll(num_of_rolls)
+    @values = (0...num_of_rolls).map {1 + rand(6)}
   end
 end
 
