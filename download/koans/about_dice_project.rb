37c6bb86e97e1655cb5bdb7e8fe78190315126b6
leslie kimm <leslie.kimm@gmail.com>
2016-02-21 20:42:36 -0800

    finish open classes

--- download/koans/about_dice_project.rb
+++ download/koans/about_dice_project.rb
@@ -2,9 +2,10 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  def initialize
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
